52c8b32c2f946dbf55b7a58e6c0d7483f23b8d85
serhatbek <serhat.bekk@gmail.com>
2022-08-22 10:15:48 +0300

    project setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { FiChevronLeft, FiChevronRight, FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 
+console.log(data);
 function App() {
   return <div className='App'></div>;
 }
