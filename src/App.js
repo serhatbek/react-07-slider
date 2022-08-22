c688c308d250840812168c2228eb0fe00f11954a
serhatbek <serhat.bekk@gmail.com>
2022-08-22 11:45:09 +0300

    slider functionality

--- src/App.js
+++ src/App.js
@@ -8,8 +8,22 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
-  // useEffect(() => {
-  // }, []);
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return clearInterval(slider);
+  }, [index]);
 
   return (
     <section className='section'>
