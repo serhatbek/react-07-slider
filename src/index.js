fcef79912035d5c62af5ce0de4e72d6ddc6298dd
serhatbek <serhat.bekk@gmail.com>
2022-08-22 13:10:55 +0300

    alternative solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import Alternative from './Alternative';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Alternative />
   </React.StrictMode>
 );
